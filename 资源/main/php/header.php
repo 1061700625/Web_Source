ea2a8211908c35214793a6c857f3a3729f36f5b0
SXF <1061700625@qq.com>
2020-02-14 12:11:08 +0800

    Add files via upload

---
+++ 资源/main/php/header.php
@@ -0,0 +1,11 @@
+<meta charset="utf-8" />
+<meta name="author" content="Songxf" />
+<title>Source</title>
+<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
+<script src="https://libs.baidu.com/jquery/1.11.3/jquery.min.js"></script>
+<style>
+  * {
+		padding:0;
+		margin:0;
+	}
+</style>
